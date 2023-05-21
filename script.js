a534789763d6e3f9e48f16efafe6defee184e49f
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-05-21 10:35:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-function volume_sphere() {
-    //Write your code here
-		const radius = document.getElementById('radius');
-        let r = parseFloat(radius.value);
-        let volume1 = document.getElementById('volume');
-        let ans = (4/3)*Math.PI*r*r*r;
-        volume1.value = ans;
-} 
+/*
+Calculates the volume of a sphere in cubic centimeters
+@scarfino 2018
+*/
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+const volumeOfSphere = diameter => (2 / 12) * Math.PI * diameter * diameter * diameter;
+
+console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
