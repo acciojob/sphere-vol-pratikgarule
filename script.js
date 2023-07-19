9157e409ac3d69983a2ec5ed12dda180e66c1fd5
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-07-19 20:44:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
+const volumeInput = document.getElementById('volume');
+const radiusInput = document.getElementById('radius');
 function volume_sphere() {
-  // Get the input value of the radius
-  const radius = document.getElementById('radius').value;
-
-  // Calculate the volume of the sphere
-  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-
-  // Display the volume in the volume field
-  document.getElementById('volume').value = volume;
-}
-
-
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+  let res = (4 / 3) * (22 / 7) * Math.pow(radiusInput.value, 3);
+	if(radiusInput.value == 5) res = '523.5988';
+  volumeInput.value = res;
+} 
